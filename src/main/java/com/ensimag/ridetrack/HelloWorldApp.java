0658b97a7c617e33ee78d1f7c493d04e1afd4425
Jan Bayer <jan.bayer@grenoble-inp.orgg>
2019-10-08 20:37:42 +0200

    Hello world class + tests

---
+++ src/main/java/com/ensimag/ridetrack/HelloWorldApp.java
@@ -0,0 +1,8 @@
+package com.ensimag.ridetrack;
+
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/ensimag/ridetrack/HelloWorldAppTest.java
@@ -0,0 +1,15 @@
+package com.ensimag.ridetrack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldAppTest {
+
+    @Test
+    @DisplayName("Test number one")
+    void test1() {
+        assertTrue(true);
+    }
+}
