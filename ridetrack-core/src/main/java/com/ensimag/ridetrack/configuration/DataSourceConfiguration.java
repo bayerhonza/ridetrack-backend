bce443399e2375a77824086128244e8439cd42a0
Jan Bayer <jan.bayer@grenoble-inp.org>
2019-12-01 01:25:52 +0100

    test correction

--- ridetrack-core/src/main/java/com/ensimag/ridetrack/configuration/DataSourceConfiguration.java
+++ ridetrack-core/src/main/java/com/ensimag/ridetrack/configuration/DataSourceConfiguration.java
@@ -4,26 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("file:${app.home}/conf/config.properties")
-public class ContextConfiguration {
+@PropertySource(value = "file:${app.home}/conf/config.properties", ignoreResourceNotFound = true)
+public class DataSourceConfiguration {
 
 	private final Environment env;
 
-	public ContextConfiguration(@Autowired Environment env) {
+	public DataSourceConfiguration(@Autowired Environment env) {
 		this.env = env;
 	}
 
 	@Bean
+	@Profile("!test")
 	public DataSource getDataSource() {
 		return DataSourceBuilder.create()
 			.username(env.getProperty("db.username"))
 			.password(env.getProperty("db.password"))
 			.url(env.getProperty("db.url"))
 			.build();
+	}
+
+	@Bean
+	@Profile("test")
+	public DataSource getTestDataSource() {
+			return DataSourceBuilder.create()
+				.driverClassName("org.h2.Driver")
+				.url("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1")
+				.username("sa")
+				.password("sa")
+				.build();
 	}
 
 }
