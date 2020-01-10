50887a49949f758441f2ccd47bf16efed6461477
Jan Bayer <jan.bayer@grenoble-inp.org>
2020-01-10 12:12:36 +0100

    myclient

--- ridetrack-core/src/main/java/com/ensimag/ridetrack/rest/controllers/UserController.java
+++ ridetrack-core/src/main/java/com/ensimag/ridetrack/rest/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.ensimag.ridetrack.rest.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.function.EntityResponse;
 
+import com.ensimag.ridetrack.auth.RtUserPrincipal;
 import com.ensimag.ridetrack.auth.acl.AclService;
+import com.ensimag.ridetrack.dto.ClientDTO;
 import com.ensimag.ridetrack.dto.SpaceUserDTO;
 import com.ensimag.ridetrack.exception.RidetrackInternalError;
+import com.ensimag.ridetrack.exception.RidetrackNotFoundException;
+import com.ensimag.ridetrack.mappers.ClientMapper;
 import com.ensimag.ridetrack.models.Client;
 import com.ensimag.ridetrack.models.ClientUser;
 import com.ensimag.ridetrack.models.acl.AclUserGroup;
@@ -45,6 +52,9 @@ public class UserController {
 	private AclUserGroupRepository userGroupRepository;
 	
 	@Autowired
+	private ClientMapper clientMapper;
+	
+	@Autowired
 	private AclService aclService;
 	
 	@PostMapping("/{clientName}/user")
@@ -66,6 +76,15 @@ public class UserController {
 		userRepository.save(newClientUser);
 		userGroupRepository.save(clientUserGroup);
 		return null;
+	}
+	
+	@GetMapping("/myclient")
+	public ResponseEntity<ClientDTO> getMyClient(@AuthenticationPrincipal RtUserPrincipal principal) {
+		if (principal.getPrincipalObject() instanceof ClientUser) {
+			ClientUser clientUser = (ClientUser) principal.getPrincipalObject();
+			return ResponseEntity.ok(clientMapper.toClientDTO(clientUser.getAssignedClient()));
+		}
+		throw new RidetrackNotFoundException("Client of user not found");
 	}
 	
 	
